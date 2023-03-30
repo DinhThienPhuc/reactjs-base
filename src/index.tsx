31dc4b7b3b31089d2d0de8c9d974eee3d060ed0f
Phuc. Dinh Thien - CMC Global DE1 <dtphuc@cmcglobal.vn>
2023-03-30 16:00:56 +0700

    refactor: stash compiling and linking bit

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,10 @@
 import * as ReactDOM from "react-dom/client";
+import * as serviceWorkerRegistration from "./serviceWorkerRegistration";
 
 import App from "./app";
 import { StrictMode } from "react";
+import { logger } from "utils";
+import reportWebVitals from "./reportWebVitals";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement,
@@ -12,3 +15,13 @@ root.render(
     <App />
   </StrictMode>,
 );
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://cra.link/PWA
+serviceWorkerRegistration.register();
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals(logger.log);
