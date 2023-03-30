af0db2046996fa82f20c0f3759446416a9ea5f0b
Phuc. Dinh Thien - CMC Global DE1 <dtphuc@cmcglobal.vn>
2023-03-30 11:38:46 +0700

    refactor: add folders

---
+++ src/utils/constants.ts
@@ -0,0 +1,26 @@
+export const ROUTES = {
+  HOME: "/",
+  ABOUT: "/about",
+  NOT_FOUND: "/not-found",
+};
+
+export const NAVBAR_ITEMS = [
+  {
+    label: "Post",
+    path: "/post",
+  },
+  {
+    label: "About",
+    path: "/about",
+  },
+  {
+    label: "Login",
+    path: "/login",
+  },
+];
+
+export const SPECIAL_VALUE = {
+  UNDEFINED_STRING: "undefined",
+  NULL_STRING: "null",
+  EMPTY_STRING: "",
+};
