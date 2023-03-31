f065f1895d0b867d3730721dd1ec948ae06dc7ba
Phuc. Dinh Thien - CMC Global DE1 <dtphuc@cmcglobal.vn>
2023-03-31 17:00:33 +0700

    fix: update name of app component to right

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 import * as ReactDOM from "react-dom/client";
 import * as serviceWorkerRegistration from "./serviceWorkerRegistration";
 
-import App from "./app";
+import App from "./appp1";
 import { StrictMode } from "react";
 import { logger } from "utils";
 import reportWebVitals from "./reportWebVitals";
