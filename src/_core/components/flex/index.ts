6ccfc5beabc9e5ed9b80fe66aa9ac378ebb49290
Phuc Thien Dinh <phuc.dinh@niteco.se>
2024-04-16 16:27:36 +0700

    Test xem thu

--- src/_core/components/flex/index.ts
+++ src/_core/components/flex/index.ts
@@ -1,7 +1,6 @@
 export * from "./_components";
 export * from "./_types";
 
-// TODO: Add hook to track render
 // TODO: Apply rhf, state case and parent control for form Element
 // TODO: Remove redundent useMemo, useCallback, useEffect
 // TODO: Add custom class for all of component's elements, base on transient props
